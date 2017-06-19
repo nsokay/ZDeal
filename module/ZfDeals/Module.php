f541d04e33e3c3bed4aeb1719e5ae8188985742e
nsokay <n.smirnovs.0612@gmail.com>
2017-06-19 14:22:53 +0300

    Prep steps done

---
+++ module/ZfDeals/Module.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ZfDeals;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . 'config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__
+                    => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function init(\Zend\ModuleManager\ModuleManager $moduleManager)
+    {
+        $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
+        $sharedEvents->attach(
+            'ZfDeals\Controller\AdminController',
+            'dispatch',
+            function ($e) {
+                $controller = $e->getTarget();
+                $controller->layout('zf-deals/layout/admin');
+            }
+        );
+    }
+}
